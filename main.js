77ff35cf423416c610924b906615d22555e34457
homxa <118491259+homxa@users.noreply.github.com>
2023-10-27 01:46:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+
+const nav = document.querySelector('.js-nav');
+const image = document.querySelector('.js-image');
+const cover = document.querySelector('.js-cover');
+cover.addEventListener('click',()=>{
+        if(nav.style.right === '-250px'){
+           nav.style.right = '0px';
+           image.src = 'menus.png';
+        }
+        else{
+                nav.style.right = '-250px';
+                image.src = 'menu.png';
+        }
+})
